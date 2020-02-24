ba11ad4186ba57f14dce70e7900bed8f9bc8d883
Ben Baumer <ben.baumer@gmail.com>
2020-02-24 14:01:00 -0500

    made cleaners tests more efficient

--- tests/testthat/test-cleaners.R
+++ tests/testthat/test-cleaners.R
@@ -8,11 +8,13 @@ test_that("cleaners work", {
 
   #test clean_wiki_names()
   #test that column names clean as expected
-  expect_equal(names(clean_wiki_names(college)), c("school", "location", "control", "type", "enrollment_2013_2014", "founded", "accreditation"))
+  vars <- c("school", "location", "control", "type", "enrollment_2013_2014", "founded", "accreditation")
+  clean_wiki <- clean_wiki_names(college)
+  expect_equal(names(clean_wiki), vars)
   #test that correctly passes arguments to janitor::clean_names()
-  expect_equal(names(clean_wiki_names(college, "all_caps")), c("SCHOOL", "LOCATION", "CONTROL", "TYPE", "ENROLLMENT_2013_2014", "FOUNDED", "ACCREDITATION"))
+  expect_equal(names(clean_wiki_names(college, "all_caps")), toupper(vars))
   #test that footnotes are removed
-  expect_equal(all(!stringr::str_detect(names(clean_wiki_names(college)), "\\[")), TRUE)
+  expect_equal(all(!stringr::str_detect(names(clean_wiki), "\\[")), TRUE)
 
 
   #test clean_rows()
@@ -23,16 +25,17 @@ test_that("cleaners work", {
 
 
   #test add_na()
-  dummy_data <- as.data.frame(rbind(c("?", "two", "three"),
-                                    c("_", "five", 7),
-                                    c("N/A", "ten", "eleven")))
+  dummy_data <- tibble::tribble(
+    ~first, ~second, ~third,
+    "?", "two", "three",
+    "_", "five", 7,
+    "N/A", "ten", "eleven"
+  )
   #see if first column is converted to NA
-  expect_equal(all(is.na(add_na(dummy_data, to_na = "N/A")$V1)), TRUE)
-
+  expect_equal(all(is.na(add_na(dummy_data, to_na = "N/A")$first)), TRUE)
 
   #test convert_types()
   expect_equal(class(convert_types(clean_rows(presidents))$Born), "Date")
-
 
   #test remove_footnotes()
   #test that footnotes are removed
