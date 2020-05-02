e7b5f1f5e96f97e5d6aee37bdfac161ed4c89fe4
Rose Porta <rporta@smith.edu>
2020-05-02 11:52:16 -0400

    tried to revise tests

--- tests/testthat/test-cleaners.R
+++ tests/testthat/test-cleaners.R
@@ -1,36 +1,40 @@
 test_that("cleaners work", {
   # sites to use for testing
-  college <- read_wiki_raw("https://en.wikipedia.org/wiki/List_of_colleges_and_universities_in_Massachusetts")
+  college <- read_wikitables("https://en.wikipedia.org/wiki/List_of_colleges_and_universities_in_Massachusetts")
 
-  presidents <- read_wiki_raw("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States_by_age")
+  presidents <- read_wikitables("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States_by_age")
 
-  marvel <- read_wiki_raw("https://en.wikipedia.org/wiki/List_of_Marvel_Cinematic_Universe_films")
+  marvel <- read_wikitables("https://en.wikipedia.org/wiki/List_of_Marvel_Cinematic_Universe_films")
 
-  ascii <- read_wiki_raw("https://en.wikipedia.org/wiki/ASCII")
+  ascii <- read_wikitables("https://en.wikipedia.org/wiki/ASCII")
 
   # test clean_wiki_names()
   # test that column names clean as expected
-  vars <- c("school", "location", "control", "type", "enrollment_2013_2014", "founded", "accreditation")
-  clean_wiki <- clean_wiki_names(college)
-  expect_equal(names(clean_wiki), vars)
+  vars <- c("school", "location", "control", "type", "enrollment", "founded", "accreditation")
+  clean_college <- clean_wiki_names(college)
+  clean_college1 <- clean_college %>% purrr::pluck(1)
+  expect_equal(names(clean_college1), vars)
 
   # test that correctly passes arguments to janitor::clean_names()
   expect_equal(
-    names(clean_wiki_names(college, "all_caps")),
+    names(clean_wiki_names(college, "all_caps") %>% purrr::pluck(1)),
     toupper(vars)
   )
 
   # test that footnotes are removed
-  expect_true(all(!stringr::str_detect(names(clean_wiki), "\\[")))
+  expect_true(all(!stringr::str_detect(names(clean_college1), "\\[")))
 
   # test that removes columns without name -- note: need to do
   # remove footnotes before this works
+  ## NOTE-- remove footnotes not working for this
   ascii_clean <- ascii %>%
-    remove_footnotes() %>%
-    clean_wiki_names()
-  expect_lt(ncol(ascii_clean), ncol(ascii))
+    #remove_footnotes() %>%
+    clean_wiki_names() %>%
+    purrr::pluck(1)
+  expect_lt(ncol(ascii_clean), ncol(ascii %>% purrr::pluck(1)))
 
   # test that removes special characters in names
+  ## clean_wiki_names won't work with dummy data-- not sure why
   dummy_data_1 <- tibble::tribble(
     ~`first@`, ~second, ~third,
     "?", "two", "three",
@@ -38,25 +42,37 @@ test_that("cleaners work", {
     "N/A", "ten", "eleven"
   )
 
-  expect_false(clean_wiki_names(dummy_data_1)[,1] %>%
+  expect_false(clean_wiki_names(dummy_data_1) %>%
+                 purrr::pluck(1) %>%
                  pull(first) %>%
                  stringr::str_detect("\\@") %>%
                  any())
 
   # test clean_rows()
   # test that duplicate of header is removed (this one has a double header)
-  expect_lt(nrow(clean_rows(presidents)), nrow(presidents))
+  # ERROR: Error in as_mapper(.f, ...) : argument ".f" is missing, with no default
+  presidents1 <- presidents %>% purrr::pluck(1)
+  expect_lt(nrow(clean_rows(presidents1)), nrow(presidents1))
 
   # test that rows with same values in all column are removed
-  expect_lt(nrow(clean_rows(marvel)), nrow(marvel))
+  ## read_wikitables not working for marvel page
+  marvel1 <- marvel %>%  purrr::pluck(1)
+  expect_lt(nrow(clean_rows(marvel1)), nrow(marvel1))
 
   # test add_na()
+  ## I'm confused about applying these functions to the dummy data because
+  ## they are supposed to take in a list of data frames
   dummy_data <- tibble::tribble(
     ~first, ~second, ~third,
     "?", "two", "three",
     "_", "five", 7,
     "N/A", "ten", "eleven"
   )
+
+  dummy_data2 <- dummy_data %>%
+    add_na(to_na = "N/A") %>%
+    purrr::pluck()
+
   # see if first column is converted to NA
   expect_true(dummy_data %>%
                 add_na(to_na = "N/A") %>%
@@ -70,9 +86,12 @@ test_that("cleaners work", {
     any(na.rm = TRUE))
 
   # test convert_types()
-  expect_is(convert_types(clean_rows(presidents))$Born, "Date")
+  ## ERROR: Error in as_mapper(.f, ...) : argument ".f" is missing, with no default
+  expect_is(convert_types(clean_rows(presidents) %>% purrr::pluck(1))$Born, "Date")
 
   # test remove_footnotes()
   # test that footnotes are removed
-  expect_true(all(!stringr::str_detect(remove_footnotes(college)$Founded, "\\[")))
+  ## ERROR: Error in colSums(!is.na(dat)) :
+  # 'x' must be an array of at least two dimensions
+  expect_true(all(!stringr::str_detect(remove_footnotes(college) %>% purrr::pluck(1)$Founded, "\\[")))
 })
